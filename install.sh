718f348331a6b132a876718a1ab9039d29895192
Árni Dagur <arni@dagur.eu>
2020-02-02 15:15:32 -0500

    More friendly to non-arch platforms

--- install.sh
+++ install.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
+set -e
 
-# -- Install patched wine --
-if grep -q "^\[thepoon\]$" /etc/pacman.conf; then
-    echo "The Poon's repo is already in /etc/pacman.conf; skipping..."
-else
-    echo "Adding The Poon's repo to /etc/pacman.conf"
-    sudo sh -c 'echo "[thepoon]
-    Server = https://archrepo.thepoon.fr
-    Server = https://mirrors.celianvdb.fr/archlinux/thepoon" >> /etc/pacman.conf'
-fi
-sudo pacman-key --keyserver hkps://hkps.pool.sks-keyservers.net -r C0E7D0CDB72FBE95
-sudo pacman-key --keyserver hkps://hkps.pool.sks-keyservers.net --lsign-key C0E7D0CDB72FBE95
-sudo pacman -Sy --needed --noconfirm wine-osu winetricks
-sudo pacman -S --needed --noconfirm curl > /dev/null # Install curl if needed
-
-# -- Install Osu! --
 export WINEPREFIX="$HOME/.osu-patch"
 export WINEARCH=win32
 
+# Exit if wine, curl, or winetricks is not installed
+wine --version > /dev/null || echo "You need to install wine"
+curl --version > /dev/null || echo "You need to install curl"
+winetricks --version > /dev/null || echo "You need to install winetricks"
+
 # Create osu-folder symlink
 echo "Creating symbolic link to the Osu! directory at ~/osu-folder ..."
-ln -s $WINEPREFIX/drive_c/users/$USER/Local\ Settings/Application\ Data/osu\! \
-    $HOME/osu-folder > /dev/null
+ln -sf $WINEPREFIX/drive_c/users/$USER/Local\ Settings/Application\ Data/osu\! \
+    $HOME/osu-folder
+
 # Install start script
 echo "Installing start script to /usr/local/bin/osu ..."
 sudo cp osu /usr/local/bin/osu
+
 # Install kill script
 echo "Installing kill script to /usr/local/bin/osukill ..."
 sudo cp osukill /usr/local/bin/osukill
-# Install Desktop shortcut
+
+# Install osu! logo
 echo "Installing Osu! logo to ~/.local/share/icons/osu!.png ..."
-curl -o $HOME/.local/share/icons/osu\!.png https://upload.wikimedia.org/wikipedia/commons/d/d3/Osu%21Logo_%282015%29.png
+mkdir -p $HOME/.local/share/icons
+cp osu_icon.png $HOME/.local/share/icons/osu\!.png
+
+# Install Desktop shortcut
 echo "Installling Osu! desktop shortcut to ~/.local/share/applications/osu!.desktop ..."
+mkdir -p $HOME/.local/share/applications
 cp osu\!.desktop $HOME/.local/share/applications/osu\!.desktop
 
 # Install the .NET framework
@@ -41,4 +39,4 @@ winetricks dotnet40
 echo "Downloading Osu! installation wizard for Windows..."
 curl -o $WINEPREFIX/osuinstall.exe https://m1.ppy.sh/r/osu\!install.exe
 echo "Running Osu! installation wizard for Windows through Wine..."
-/opt/wine-osu/bin/wine $WINEPREFIX/osuinstall.exe
+wine $WINEPREFIX/osuinstall.exe
